6b06873e02038857b82e6ad323393b3d5df1f510
Nordin <nordin@digivox.nl>
2019-03-06 11:15:31 +0100

    Initial Commit

---
+++ ModalViewExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModalViewExample
+//
+//  Created by Nordin on 06/03/2019.
+//  Copyright © 2019 Digivox BV. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ModalViewExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModalViewExample
+//
+//  Created by Nordin on 06/03/2019.
+//  Copyright © 2019 Digivox BV. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
